17962e134ad6eaa99df201798f81b8212d60a626
Jasson <jasson198516@gmail.com>
2025-11-17 15:14:54 -0500

    Primera revision Parcial

---
+++ src/WebApi/CreateOrderUseCase.cs
@@ -0,0 +1,12 @@
+﻿
+internal class CreateOrderUseCase
+{
+    public CreateOrderUseCase()
+    {
+    }
+
+    internal object Execute(string customer, string product, int qty, decimal price)
+    {
+        throw new NotImplementedException();
+    }
+}
